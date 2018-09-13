4b9b997ada31b8ddbfa985752a113c31c79061c6
Pawel <tester1464@gmail.com>
2018-09-13 14:36:53 +0200

    Populated files

---
+++ Delagates/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delagates
+{
+    class Car
+    {
+
+        //field with delegate 
+        public NameChangedDelegate NameChanged;
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    var oldName = _name;
+                    _name = value;
+                    if (NameChanged != null)
+                        NameChanged(oldName, _name);
+                }
+                
+            }
+        }
+
+    }
+}
---
+++ Delagates/NameChangedDelegate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delagates
+{
+    //delagate with signature:
+    // return type - void
+    // arguments - 2x string
+    public delegate void NameChangedDelegate(string oldName, string newName);
+}
